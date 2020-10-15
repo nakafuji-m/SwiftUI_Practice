fbf65211a7d33b206c9c5bd4500cbde2527dd516
Nakafuji Mizuto <2180417@ecccomp.com>
2020-10-15 23:03:20 +0900

    Initial Commit

---
+++ SwiftUI_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Practice
+//
+//  Created by Nakafuji on 2020/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
